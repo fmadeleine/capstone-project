af920880fa20eabc42a67a3188f4f83c235e1b54
Madeleine Frister <madeleine.f136@gmail.com>
2023-02-07 11:36:05 +0000

    html setup

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <img src="images/logo-secondary.png" alt="little lemon logo"></img>
+                <div>
+                    <h4>Navigation</h4>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Menu</a></li>
+                        <li><a href="">Reservations</a></li>
+                        <li><a href="">Order Online</a></li>
+                        <li><a href="">Login</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Contact</h4>
+                    <ul>
+                        <li>Address</li>
+                        <li>Phone Number</li>
+                        <li>Email</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Social Media</h4>
+                    <ul>
+                        <li><a href="">Facebook</a></li>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+  }
+
+  export default Footer;
+
---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+
+
+function Nav() {
+    return (
+        <nav>
+            <div>
+                <img src="images/logo.png" alt="little lemon logo"/>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Menu</a></li>
+                    <li><a href="">Reservations</a></li>
+                    <li><a href="">Order Online</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </div>
+
+        </nav>
+    );
+  }
+
+  export default Nav;
+
